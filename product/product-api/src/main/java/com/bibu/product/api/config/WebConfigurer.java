bca509adcfd7e4cb11f930a1bf3bf201d4368ebd
xieyi <63896020@qq.com>
2024-07-21 23:07:41 +0800

    Revert "项目成立"
    This reverts commit be11d1a1

--- product/product-api/src/main/java/com/bibu/product/api/config/WebConfigurer.java
+++ product/product-api/src/main/java/com/bibu/product/api/config/WebConfigurer.java
@@ -2,6 +2,7 @@ package com.bibu.product.api.config;
 
 import com.bibu.product.api.interceptor.UserTokenCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Author XY
  * Date 2024/5/6 下午6:14
  */
-//@Configuration
+@Configuration
 public class WebConfigurer implements WebMvcConfigurer {
 
     @Autowired
