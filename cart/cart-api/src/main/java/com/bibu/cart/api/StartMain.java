546a78281a53c47a1697495d0c7236a57a07265d
Bome <63896020@qq.com>
2026-02-28 00:11:17 +0800

    Create StartMain.java with MapperScan and EnableDiscoveryClient annotations

--- cart/cart-api/src/main/java/com/bibu/cart/api/StartMain.java
+++ cart/cart-api/src/main/java/com/bibu/cart/api/StartMain.java
@@ -1,10 +1,15 @@
 package com.bibu.cart.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@MapperScan("com.bibu.cart.api.mapper")
+@EnableDiscoveryClient
 public class StartMain {
+
     public static void main(String[] args) {
         SpringApplication.run(StartMain.class, args);
     }
